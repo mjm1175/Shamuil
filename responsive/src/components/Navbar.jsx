b7959444d330a63ebc751e1a59bdf959098eee8a
Maria Morejon <79123420+mjm1175@users.noreply.github.com>
2024-01-03 13:10:08 -0600

    Finished Navbar

---
+++ responsive/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import { Link, NavLink } from 'react-router-dom'
+import './navbar.css'
+import Logo from '../images/logo.png'
+import {links} from '../data'
+import { FaBars } from 'react-icons/fa6'
+import { MdOutlineClose } from 'react-icons/md'
+import { useState } from 'react'
+
+
+const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+
+  return (
+    <nav>
+        <div className="container nav__container">
+            <Link to="/" className='logo'>
+                <img src={Logo} alt="Nav Logo" />
+            </Link>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}>
+                {
+                    links.map(({name, path}, index) => {
+                        return (
+                            <li key={index}>
+                                <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{name}</NavLink>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <button className='nav__toggle-btn' onClick={() => setIsNavShowing(!isNavShowing)}>
+                {
+                    isNavShowing ? <MdOutlineClose/> : <FaBars/>
+                }
+            </button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ responsive/src/pages/pageNotFound/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import './pageNotFound.css'
+
+const PageNotFound = () => {
+  return (
+    <div>PageNotFound</div>
+  )
+}
+
+export default PageNotFound
