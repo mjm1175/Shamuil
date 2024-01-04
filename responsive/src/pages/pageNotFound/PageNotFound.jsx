8a958c8db50e396624d4b26f8e65059b65458ef1
Maria Morejon <79123420+mjm1175@users.noreply.github.com>
2024-01-04 12:55:43 -0600

    Responsive finished

--- responsive/src/pages/pageNotFound/PageNotFound.jsx
+++ responsive/src/pages/pageNotFound/PageNotFound.jsx
@@ -1,8 +1,14 @@
+import { Link } from 'react-router-dom'
 import './pageNotFound.css'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className='btn'>Home</Link>
+      </div>
+    </section>
   )
 }
 
